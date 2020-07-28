5daeda1998bdb8e51daa2a25b937f3e223b2825b
Jose Carvalho <jose.carvalho-ext@man.eu>
2020-07-28 20:17:55 +0100

    refactor Koa dependencies

--- src/app.js
+++ src/app.js
@@ -1,22 +1,45 @@
-import koa from 'koa';
-import router from 'koa-router';
-import logger from 'koa-logger';
-import route from 'koa-router';
+const Koa = require('koa');
+const Router = require('@koa/router');
+var logger = require('koa-logger');
 
-
-var app = new koa();
+const app = new Koa();
+const router = new Router();
 
 // Init logger
 app.use(logger());
-app.use(route(app));
+
+router.get('/', (ctx, next) => {
+    // ctx.router available
+    ctx.body = 'hello world!'
+});
+
+router.get('/home', (ctx, next) => {
+    // ctx.router available
+    ctx.body = 'HOME!'
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+//var _ = router(); //Instantiate the router
+
+//_.get('/', async ctx => {
+//ctx.body = 'Hello World';
+//});
+//_.get('/hello', async ctx => {
+//ctx.body = 'Hello hello!';
+//});
+//app.use(_.routes()); //Use the routes defined using the router
 
 //routes
-app.get('/', async ct => {
-    ctx.body = 'Hello World';
-})
-app.get('/test', async ct => {
-    ctx.body = 'Hello Test';
-})
+//app.get('/', async ct => {
+//ctx.body = 'Hello World';
+//});
+
+//app.get('/test', async ct => {
+//ctx.body = 'Hello Test';
+//});
 
 //spin up
 var port = process.env.PORT || 3000;
